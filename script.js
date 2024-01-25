2119088224eca14aea2d7a173cdc0776e614af13
Gonza <goesterheld@fi.uba.ar>
2024-01-25 18:13:07 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
+
+document.getElementById('backtrackButton').addEventListener('click', function() {
+    window.history.back();
+}
+); 
