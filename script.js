af63708d6aca599ff9f8582211961b146d1bccaf
Gonza <goesterheld@fi.uba.ar>
2024-02-01 13:46:20 -0300

    .

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ window.addEventListener("load", function(){
     loader.style.display = "none";
 })
 
-document.getElementById('backtrackButton').addEventListener('click', function() {
-    window.history.back();
-}
-); 
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }else {
+      entry.target.classList.remove("show");
+    }
+  })
+  
+});
+
+const mainName = document.querySelectorAll(".main-name");
+mainName.forEach((el) => observer.observe(el));
+
+const secName = document.querySelectorAll(".main-title");
+secName.forEach((el) => observer.observe(el));
