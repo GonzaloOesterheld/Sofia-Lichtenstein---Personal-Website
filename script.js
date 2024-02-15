64b45d0909c7f99ecf9d72e711a8125b8d0e1b72
Gonza <goesterheld@fi.uba.ar>
2024-02-15 16:17:59 -0300

    .

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-})
+//var loader = document.getElementById("preloader");
+//window.addEventListener("load", function(){
+//    loader.style.display = "none";
+//})
 
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }else {
-      entry.target.classList.remove("show");
-    }
-  })
-  
-});
+//const observer = new IntersectionObserver((entries) => {
+//  entries.forEach((entry) => {
+//    if(entry.isIntersecting) {
+//      entry.target.classList.add("show");
+//   }else {
+//      entry.target.classList.remove("show");
+//   }
+//  })
+//  
+//});
 
-const mainName = document.querySelectorAll(".main-name");
-mainName.forEach((el) => observer.observe(el));
+//const mainName = document.querySelectorAll(".main-name");
+//mainName.forEach((el) => observer.observe(el));
 
-const secName = document.querySelectorAll(".main-title");
-secName.forEach((el) => observer.observe(el));
+//const secName = document.querySelectorAll(".main-title");
+//secName.forEach((el) => observer.observe(el));
+
+
